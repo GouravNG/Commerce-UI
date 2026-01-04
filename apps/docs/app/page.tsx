640b5c44fec317c9646bbae4403e9a12b7d392e3
Gourav <gouravgunaga555@gmail.com>
2026-01-04 11:25:51 +0530

    chore: Usage check for MUI library

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "commerce-ui-scn/components/ui/button"
-
+import { Button as Button2, Button3 } from "commerce-ui-mui"
 export default function Home() {
   return (
     <>
       <h1 className="bg-blue-500 p-2 m-2 ">Commerce-UI</h1>
       <Button variant={"outline"}>Commerce UI button</Button>
+      <Button2 variant="contained">MUI Button</Button2>
+      <Button3 />
     </>
   )
 }
